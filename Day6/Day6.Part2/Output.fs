01452d695922566390f133f10346646d3f1d65c2
Connel Hooley <connelhooley@gmail.com>
2016-12-19 20:22:07 +0000

    started day 6 part 2

---
+++ Day6/Day6.Part2/Output.fs
@@ -0,0 +1,16 @@
+﻿module Output
+
+open System
+open Helpers
+
+let mostCommon (items:seq<string>) =
+    items
+    |> Seq.countBy id
+    |> Seq.sortBy fst
+    |> Seq.maxBy snd
+    |> fst
+
+let decode input =
+    input
+    |> Seq.map mostCommon
+    |> mergeStrings
