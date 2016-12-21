e964b621ecc794266edfadb2056c13a82f427e2f
Connel Hooley <connelhooley@gmail.com>
2016-12-21 21:30:33 +0000

    Finished day 7 part 2

---
+++ Day7/Day7.Part2/Network.fs
@@ -0,0 +1,48 @@
+﻿module Network
+    
+open System
+open Helpers
+
+let private getInsideBrackets input =
+    input
+    |> regexGroups @"[^\[\]]+(?=])"
+
+let private getOutsideBrackets input =
+    input
+    |> regexGroups @"\w+(?=\[|\s|$)"
+
+let private invertAba aba =
+    let chars = toCharArray aba
+    sprintf "%c%c%c" chars.[1] chars.[0] chars.[1]
+
+let private getAba (chars:char[]) i =
+    let first = chars.[i];
+    let second = chars.[i+1];
+    let third = chars.[i+2];
+    if(first = third && first <> second) then
+        Some(sprintf "%c%c%c" first second third)
+    else
+        None
+
+let private getAbas input =
+    let chars = toCharArray input
+    [0..chars.Length-3]
+    |> Seq.map (getAba chars)
+    |> Seq.choose id
+    
+let private containsAnyOf required input =
+    required
+    |> Seq.exists (contains input)
+ 
+let private ipSupportsSsl ipAddress =
+    let insideBrackets = getInsideBrackets ipAddress
+    let outsideBrackets = getOutsideBrackets ipAddress
+    let abas = insideBrackets |> Seq.collect getAbas
+    let possibleBabs = abas |> Seq.map invertAba
+    outsideBrackets
+    |> Seq.exists (containsAnyOf possibleBabs)
+    
+let countValid (input:seq<string>) =
+    input
+    |> Seq.filter ipSupportsSsl
+    |> Seq.length
---
+++ Day7/Day7.Part2/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv = 
+    File.ReadAllLines "input.txt"
+    |> Network.countValid
+    |> printfn "%A"
+    ignore(Console.ReadLine())
+    0 // return an integer exit code
