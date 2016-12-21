944fa2c5c3c2df8a7007aa46f11392278eaf10ba
Connel Hooley <connelhooley@gmail.com>
2016-12-21 21:30:33 +0000

    finished day 7 part 2

---
+++ Day7/Day7.Part2/Helpers.fs
@@ -0,0 +1,27 @@
+﻿module Helpers
+
+open System
+open System.Text.RegularExpressions
+
+let toString x = 
+    x.ToString()
+
+let toCharArray (x:string) = 
+    x.ToCharArray()
+
+let contains (haystack:string) (needle:string) = 
+    haystack.Contains(needle)
+
+let private matchesToList (matchCollection:MatchCollection) =
+    [for regMatch in matchCollection -> regMatch]
+
+let private groupsToList (groupCollection:GroupCollection) =
+    [for regGroup in groupCollection -> regGroup]
+
+let regexGroups regExp input =
+    let regex = Regex regExp
+    regex.Matches input
+    |> matchesToList
+    |> List.map (fun m -> m.Groups)
+    |> List.collect groupsToList
+    |> List.map toString
