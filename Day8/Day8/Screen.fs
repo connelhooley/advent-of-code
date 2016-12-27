a1ac72ec974e809b7cbf70964cb47ffb343a1d19
Connel Hooley <connelhooley@gmail.com>
2016-12-27 22:17:09 +0000

    cleaned up day 8

--- Day8/Day8/Screen.fs
+++ Day8/Day8/Screen.fs
@@ -9,45 +9,41 @@ module ScreenModule =
     let perform screen instruction =
         
         let getRotationInput amount source = 
-            let createInput length pixelIndexesToSwitchOn =
+            let length = Array.length source
+            let createInput pixelIndexesToSwitchOn =
                 let result = Array.create length false
                 let turnOnPixel index = 
                      Array.set result index true
                 Array.iter turnOnPixel pixelIndexesToSwitchOn
                 result
-            let shiftPixel amount length current =
+            let shiftPixel current =
                 if amount = 0 then current
                 else (current + amount) % length
-            let length = Array.length source
             source
-            |> Array.mapi (fun i p -> (i, p))
+            |> Array.mapi (fun index pixel -> index, pixel)
             |> Array.filter snd
-            |> Array.map (fst >> shiftPixel amount length)
-            |> createInput length
+            |> Array.map (fst >> shiftPixel)
+            |> createInput
 
         match instruction with
         | Rectangle(width, height) -> 
             let input = Array2D.create height width true
             Array2D.blit input 0 0 screen 0 0 height width
         | RotateColumn(column, amount) ->
-            let transpose array = seq {
-                for i in array do
-                    yield seq { yield i }
-            }
-            let height = screen 
-                         |> Array2D.length1
             let input = screen.[*, column]
                         |> getRotationInput amount
-                        |> transpose
+                        |> Array.map Array.singleton
                         |> array2D
+            let height = screen
+                         |> Array2D.length1
             Array2D.blit input 0 0 screen 0 column height 1
         | RotateRow(row, amount) ->
-            let width = screen
-                        |> Array2D.length2
             let input = screen.[row, *]
                         |> getRotationInput amount
                         |> Array.singleton
                         |> array2D
+            let width = screen
+                        |> Array2D.length2
             Array2D.blit input 0 0 screen row 0 1 width
         screen
     
