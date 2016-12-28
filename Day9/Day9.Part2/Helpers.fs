4cbdfde07a72081d1f8bb246beff2c84da30bac3
Connel Hooley <connelhooley@gmail.com>
2016-12-28 19:10:41 +0000

    started day 9 part 2

---
+++ Day9/Day9.Part2/Helpers.fs
@@ -0,0 +1,18 @@
+﻿module Helpers
+
+open System
+
+let trim (x:string) =
+    x.Trim()
+
+let toCharArray (x:string) =
+    x.ToCharArray()
+    
+let toString x =
+    x.ToString()
+    
+let toLower (x:string) =
+    x.ToLower()
+
+let split (seperator:string) (x:string) =
+    x.Split([|seperator|], StringSplitOptions.RemoveEmptyEntries)
---
+++ Day9/Day9.Part2/Program.fs
@@ -0,0 +1,13 @@
+﻿open Compression
+
+open System
+open System.IO
+
+[<EntryPoint>]
+let main argv = 
+    File.ReadAllText "input.txt"
+    |> CompressionModule.parseInput
+    |> CompressionModule.decompress
+    |> CompressionModule.printOutput
+    ignore(Console.ReadLine())
+    0 // return an integer exit code
