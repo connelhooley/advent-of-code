e0ece9ecc27f08f0e75409f91b2738a480cc5c4b
Connel Hooley <connelhooley@gmail.com>
2017-12-01 21:04:53 +0000

    Day1.Part1

---
+++ 2017/Day1/Day1.Part1/Captcha.fs
@@ -0,0 +1,13 @@
+﻿module Captcha
+
+let parse (input:string) = 
+    seq {
+        yield! input
+        yield input.[0]
+    }
+    |> Seq.pairwise
+    |> Seq.filter (fun (a, b) -> a = b)
+    |> Seq.map fst
+    |> Seq.map (sprintf "%c")
+    |> Seq.map int
+    |> Seq.sum
