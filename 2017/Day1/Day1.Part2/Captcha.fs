fa802027c167b188bce72084bc539597b7927267
Connel Hooley <connelhooley@gmail.com>
2017-12-01 23:27:18 +0000

    Day1.Part2

---
+++ 2017/Day1/Day1.Part2/Captcha.fs
@@ -0,0 +1,15 @@
+﻿module Captcha
+
+let parse (input:string) =     
+    let rec circular = seq {
+        yield! [0..input.Length-1]
+        yield! circular
+    }
+    [0 .. input.Length-1]
+    |> Seq.map (fun i -> (Seq.item i circular, Seq.item (i + input.Length/2) circular))
+    |> Seq.map (fun (a, b) -> (input.[a], input.[b]))
+    |> Seq.filter (fun (a, b) -> a = b)
+    |> Seq.map fst
+    |> Seq.map (sprintf "%c")
+    |> Seq.map int
+    |> Seq.sum
