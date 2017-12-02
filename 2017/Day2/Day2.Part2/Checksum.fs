a23e5d83d0937df40cf341a7c90c5a90376e1ed7
Connel Hooley <connelhooley@gmail.com>
2017-12-02 13:43:59 +0000

    Started Day2.Part2

---
+++ 2017/Day2/Day2.Part2/Checksum.fs
@@ -0,0 +1,15 @@
+﻿module Checksum
+
+let parse input =
+    let cartesian items = 
+        items |> Array.collect (fun x -> items |> Array.map (fun y -> x, y))
+    
+    input
+    |> Array.map cartesian
+    |> Array.map (fun row -> 
+        row 
+        |> Array.map (fun (a,b) -> a / b)
+        |> Array.filter (fun i -> i <> 1)
+        |> Array.filter (fun i -> i % 2  = 0))
+    
+    //|> Array.sum
