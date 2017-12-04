a568702cab7043424448fec00be9643ad419f709
Connel Hooley <connelhooley@gmail.com>
2017-12-04 22:41:03 +0000

    Finished Day4.Part2

---
+++ 2017/Day4/Day4.Part2/Passphrase.fs
@@ -0,0 +1,27 @@
+﻿module Passphrase
+
+let isValid (passphrase: string) = 
+    let isMatch ((word1:string), (word2:string)) =
+        let string1 = 
+            word1.ToCharArray()
+            |> Array.sort
+            |> Array.map string
+            |> String.concat ""
+        let string2 = 
+            word2.ToCharArray()
+            |> Array.sort
+            |> Array.map string
+            |> String.concat ""
+        string1 = string2
+        
+    let words = passphrase.Split()
+    let unique = 
+        words 
+        |> Array.distinct
+    if unique.Length < words.Length then
+        false
+    else
+        Array.allPairs words words
+        |> Array.filter (fun (a, b) -> a <> b)
+        |> Array.exists isMatch
+        |> not
---
+++ 2017/Day4/Day4.Part2/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main _ = 
+    File.ReadAllLines "input.txt"
+    |> Array.filter Passphrase.isValid
+    |> Array.length
+    |> printfn "%A"
+    ignore(Console.ReadLine())
+    0 // return an integer exit code
