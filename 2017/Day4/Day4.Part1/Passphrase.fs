2a837d1be47ae7c6cd9c0eb74e8374ab0ee4de9c
Connel Hooley <connelhooley@gmail.com>
2017-12-04 08:52:40 +0000

    Finished Day4.Part1

---
+++ 2017/Day4/Day4.Part1/Passphrase.fs
@@ -0,0 +1,8 @@
+﻿module Passphrase
+
+let isValid (input: string) = 
+    let words = 
+        input.Split()
+        |> Array.map(fun s -> s.Trim())
+    
+    (words |> Array.length) = (words |> Array.distinct |> Array.length)
