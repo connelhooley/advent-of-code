3b8330c541e9d6681642f84e6aa8124fb1e7be3f
Connel Hooley <connelhooley@gmail.com>
2017-12-05 10:45:20 +0000

    Tidy up

--- 2017/Day4/Day4.Part2/Passphrase.fs
+++ 2017/Day4/Day4.Part2/Passphrase.fs
@@ -1,24 +1,14 @@
 ﻿module Passphrase
 
 let isValid (passphrase: string) = 
+    
     let isMatch ((word1:string), (word2:string)) =
-        let string1 = 
-            word1.ToCharArray()
-            |> Array.sort
-            |> Array.map string
-            |> String.concat ""
-        let string2 = 
-            word2.ToCharArray()
-            |> Array.sort
-            |> Array.map string
-            |> String.concat ""
-        string1 = string2
+        let sort (s:string) = Array.sort (s.ToCharArray())
+        sort word1 = sort word2
         
     let words = passphrase.Split()
-    let unique = 
-        words 
-        |> Array.distinct
-    if unique.Length < words.Length then
+    let distinctWords = Array.distinct words
+    if distinctWords.Length <> words.Length then
         false
     else
         Array.allPairs words words
