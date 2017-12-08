d7c94b2db70262983056e3e80a3e48add693bd79
Connel Hooley <connelhooley@gmail..com>
2017-12-08 23:31:29 +0000

    Finished Day6.Part1

---
+++ 2017/Day6/Day6.Part1/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    Reallocation.read "input.txt"
+    |> Reallocation.countReallocates
+    |> Console.WriteLine
+    ignore(Console.ReadLine())
+    0 // return an integer exit code
---
+++ 2017/Day6/Day6.Part1/Reallocation.fs
@@ -0,0 +1,43 @@
+﻿module Reallocation
+
+open System.IO
+open System
+
+type private State = {
+    History: int[][]
+
+}
+
+let read fileName =
+    File.ReadAllText(fileName).Split()
+    |> Array.filter (not << String.IsNullOrWhiteSpace)
+    |> Array.map int
+
+let countReallocates memoryBanks = 
+    let rec loop count previous =
+        let indexToReallocate =
+            memoryBanks
+            |> Array.mapi (fun i x -> (i, x))
+            |> Array.sortByDescending snd
+            |> Array.item 0
+            |> fst
+        let valueToReallocate = memoryBanks.[indexToReallocate]
+    
+        let rec indexSeq = seq {
+            yield! [indexToReallocate+1 .. memoryBanks.Length-1]
+            yield! [0 .. indexToReallocate]
+            yield! indexSeq
+        }
+
+        memoryBanks.[indexToReallocate] <- 0
+        for i in Seq.take valueToReallocate indexSeq do 
+            memoryBanks.[i] <- memoryBanks.[i]+1
+                
+        let updatedMemoryBanks = List.ofArray memoryBanks
+
+        if List.contains updatedMemoryBanks previous then
+            count
+        else
+            loop (count+1) (updatedMemoryBanks :: previous)
+    
+    loop 1 List.empty
