0cfec830c56833b7269a6a9478ba1fa365ab04c2
Connel Hooley <connelhooley@gmail..com>
2017-12-10 10:26:34 +0000

    Finished Day6.Part2

---
+++ 2017/Day6/Day6.Part2/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    //Reallocation.read "input.txt"
+    [0;2;7;0]
+    |> Reallocation.countReallocateLoopSize
+    |> Console.WriteLine
+    ignore(Console.ReadLine())
+    0 // return an integer exit code
---
+++ 2017/Day6/Day6.Part2/Reallocation.fs
@@ -0,0 +1,45 @@
+﻿module Reallocation
+
+open System.IO
+open System
+
+let read fileName =
+    File.ReadAllText(fileName).Split()
+    |> Array.filter (not << String.IsNullOrWhiteSpace)
+    |> Array.map int
+    |> List.ofArray
+
+let countReallocateLoopSize memoryBanks = 
+    let reallocate index value banks =
+        let rec indexSeq = seq {
+            yield! [index+1 .. (List.length banks)-1]
+            yield! [0 .. index]
+            yield! indexSeq
+        }
+
+        let memoryBankArray = Array.ofList banks
+
+        memoryBankArray.[index] <- 0
+        for i in Seq.take value indexSeq do 
+            memoryBankArray.[i] <- memoryBankArray.[i]+1
+        
+        List.ofArray memoryBankArray
+
+    let findPrevious previousMemoryBanks updatedMemoryBanks =
+        previousMemoryBanks 
+            |> List.rev
+            |> List.tryFindIndex ((=) updatedMemoryBanks)
+        
+    let rec loop count (previousMemoryBanks: int list list) (currentMemoryBanks: int list) =
+        let indexToReallocate, valueToReallocate =
+            currentMemoryBanks
+            |> List.indexed
+            |> List.sortByDescending snd
+            |> List.item 0
+        let updatedMemoryBanks = reallocate indexToReallocate valueToReallocate currentMemoryBanks
+        
+        match findPrevious previousMemoryBanks updatedMemoryBanks with
+        | Some foundIndex -> count-foundIndex-1
+        | None -> loop (count+1) (updatedMemoryBanks :: previousMemoryBanks) updatedMemoryBanks
+            
+    loop 1 List.empty memoryBanks
