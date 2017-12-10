438a28f5beb60c739985e065c8da20bd007bddc0
Connel Hooley <connelhooley@gmail..com>
2017-12-10 15:00:46 +0000

    Tidy up

--- 2017/Day7/Day7.Part1/Tower.fs
+++ 2017/Day7/Day7.Part1/Tower.fs
@@ -11,16 +11,17 @@ type Program = {
 
 let read fileName =
     let matchLine line =
-        let parseChildren (input:string) =
-            input.Split(',')
-            |> Array.map (fun s -> s.Trim())
-            |> Array.filter (not << System.String.IsNullOrWhiteSpace)
-            |> List.ofArray            
         let _match = Regex.Match(line, "([a-z]+) \((\d*)\)[ ->]*([a-z ,]*)")
         {
-            Name = _match.Groups.[1].Value
-            Weight = _match.Groups.[2].Value |> int
-            Children = _match.Groups.[3].Value |> parseChildren
+            Name = 
+                _match.Groups.[1].Value
+            Weight = 
+                _match.Groups.[2].Value |> int
+            Children = 
+                if System.String.IsNullOrWhiteSpace _match.Groups.[3].Value then
+                    List.empty
+                else
+                    List.ofArray (_match.Groups.[3].Value.Split ", ")
         }
 
     fileName
@@ -29,18 +30,12 @@ let read fileName =
     |> List.ofArray
 
 let findRoot programs = 
-    
-    let isLeafNode programs program =
+    let hasNoParent program = 
         programs
-        |> List.exists (fun p -> p.Children |> List.contains program.Name)
+        |> List.map (fun p -> p.Children)
+        |> List.exists (fun children -> children |> List.contains program.Name)
         |> not
-
-    let rec filter programs =
-        let filtered = programs |> List.filter (isLeafNode programs)
-        if filtered.Length = 1 
-        then
-            filtered.Head.Name
-        else 
-            filter filtered
-    
-    filter programs
+    let root = 
+        programs 
+        |> List.find hasNoParent
+    root.Name
