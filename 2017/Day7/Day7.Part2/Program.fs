bbc88d7fc86a329fb3b9aca745a3454a4775016a
Connel Hooley <connelhooley@gmail..com>
2017-12-10 15:36:16 +0000

    Started Day7.Part2

---
+++ 2017/Day7/Day7.Part2/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    Tower.read "input.txt"
+    |> Tower.findRoot
+    |> Console.WriteLine
+    ignore(Console.ReadLine())
+    0 // return an integer exit code
---
+++ 2017/Day7/Day7.Part2/Tower.fs
@@ -0,0 +1,40 @@
+﻿module Tower
+
+open System.IO
+open System.Text.RegularExpressions
+
+type Program = {
+    Name: string
+    Weight: int
+    Children: string list
+}
+
+let read fileName =
+    let matchLine line =
+        let _match = Regex.Match(line, "([a-z]+) \((\d*)\)[ ->]*([a-z ,]*)")
+        {
+            Name = 
+                _match.Groups.[1].Value
+            Weight = 
+                _match.Groups.[2].Value |> int
+            Children = 
+                if System.String.IsNullOrWhiteSpace _match.Groups.[3].Value then
+                    List.empty
+                else
+                    List.ofArray (_match.Groups.[3].Value.Split ", ")
+        }
+
+    fileName
+    |> File.ReadAllLines
+    |> Array.map matchLine
+    |> List.ofArray
+
+let findRoot programs = 
+    let hasNoParent program = 
+        programs
+        |> List.exists (fun p -> p.Children |> List.contains program.Name)
+        |> not
+    let root = 
+        programs 
+        |> List.find hasNoParent
+    root.Name
