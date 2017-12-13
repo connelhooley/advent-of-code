bc83de21139ec02fbcd924fee9a63f71667ea761
Connel Hooley <connelhooley@gmail..com>
2017-12-13 21:03:08 +0000

    Finished Day9.Part1

---
+++ 2017/Day9/Day9.Part1/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    Stream.read "input.txt"
+    |> Stream.countGroupScores
+    |> printfn "%i"
+    ignore(Console.ReadLine())
+    0 // return an integer exit code
---
+++ 2017/Day9/Day9.Part1/Stream.fs
@@ -0,0 +1,50 @@
+﻿module Stream
+
+open System.IO
+
+type private State = {
+    Depth: int
+    TotalScore: int
+    IgnoreCurrent: bool
+    IsInsideGarbage: bool
+}
+
+let read fileName = (File.ReadAllText fileName).ToCharArray()
+
+let countGroupScores stream =
+    let processItem state item =
+        match state with
+        | state when state.IgnoreCurrent -> 
+            { state with IgnoreCurrent = false }
+        | state when state.IsInsideGarbage -> 
+            match item with
+            | '!' -> { state with IgnoreCurrent = true }
+            | '>' -> { state with IsInsideGarbage = false }
+            | _ -> state
+        | _ ->
+            match item with
+            | '!' -> { state with IgnoreCurrent = true }
+            | '<' -> { state with IsInsideGarbage = true }
+            | '{' -> 
+                { state with 
+                    Depth = state.Depth + 1
+                    TotalScore = state.TotalScore +  state.Depth + 1
+                }
+            | '}' -> 
+                { state with 
+                    Depth = state.Depth - 1
+                }
+            | _ -> state
+        
+    let initalState = {
+        Depth = 0;
+        TotalScore = 0;
+        IgnoreCurrent = false
+        IsInsideGarbage = false
+    }
+
+    let result = 
+        stream
+        |> Array.fold processItem initalState
+    
+    result.TotalScore
