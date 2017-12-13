9a99c2a26153266def9798a34593cfcdc5ca2520
Connel Hooley <connelhooley@gmail..com>
2017-12-13 21:11:46 +0000

    Finished Day9.Part2

---
+++ 2017/Day9/Day9.Part2/Stream.fs
@@ -0,0 +1,41 @@
+﻿module Stream
+
+open System.IO
+
+type private State = {
+    TotalGarbageCount: int
+    IgnoreCurrent: bool
+    IsInsideGarbage: bool
+}
+
+let read fileName = (File.ReadAllText fileName).ToCharArray()
+
+let countItemsInGarbage stream =
+    let processItem state item =
+        match state with
+        | state when state.IgnoreCurrent -> 
+            { state with IgnoreCurrent = false }
+        | state when state.IsInsideGarbage -> 
+            match item with
+            | '!' -> { state with IgnoreCurrent = true }
+            | '>' -> { state with IsInsideGarbage = false }
+            | _ -> { state with TotalGarbageCount = state.TotalGarbageCount + 1 }
+        | _ ->
+            match item with
+            | '!' -> { state with IgnoreCurrent = true }
+            | '<' -> { state with IsInsideGarbage = true }
+            | '{' -> state
+            | '}' -> state
+            | _ -> state
+        
+    let initalState = {
+        TotalGarbageCount = 0;
+        IgnoreCurrent = false
+        IsInsideGarbage = false
+    }
+
+    let result = 
+        stream
+        |> Array.fold processItem initalState
+    
+    result.TotalGarbageCount
