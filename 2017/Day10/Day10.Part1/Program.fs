4b2f7d3487d57e22bd38f9da272bde49bbe127df
Connel Hooley <connelhooley@gmail..com>
2017-12-20 18:56:54 +0000

    Tidy up

--- 2017/Day10/Day10.Part1/Program.fs
+++ 2017/Day10/Day10.Part1/Program.fs
@@ -5,9 +5,7 @@ open System
 [<EntryPoint>]
 let main _ =
     "input.txt"
-    //"input2.txt"
     |> KnotHash.parse
-    //|> KnotHash.perform [0 .. 4]
     |> KnotHash.perform [0 .. 255]
     |> KnotHash.multiplyFirstTwoNumbers
     |> printf "%i"
