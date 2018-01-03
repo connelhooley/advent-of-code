5b8995561e9d750b7611b49bdac05a49e00ed45f
Connel Hooley <connelhooley@gmail..com>
2018-01-03 21:32:07 +0000

    Started Day11.Part2

---
+++ 2017/Day11/Day11.Part2/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.IO
+
+[<EntryPoint>]
+let main _ =
+    //"ne,ne,ne" //3
+    //"ne,ne,sw,sw" //0
+    //"ne,ne,s,s" //2
+    //"se,sw,se,sw,sw" //3
+    "input.txt"
+    |> File.ReadAllText
+    |> HexGrid.parseInput
+    |> HexGrid.getStepsCount
+    |> printfn "%A" 
+    ignore(Console.ReadLine())
+    0 // return an integer exit code
+        
